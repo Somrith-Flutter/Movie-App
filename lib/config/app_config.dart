c53aedf95f812dc329e9f7051eb62b1d116c5120
lyhoung <pov.lyhoung@e-power.com.kh>
2024-06-11 12:05:50 +0700

    _+

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:legend_cinema/config/app_controller.dart';
+import 'package:legend_cinema/config/themes/app_theme.dart';
+import 'package:legend_cinema/config/themes/dark_theme.dart';
 import 'package:legend_cinema/constants/app_constant.dart';
 import 'package:legend_cinema/core/splash_screen.dart';
+import 'package:legend_cinema/translation/generated/l10n.dart';
 
 class LegendCinemaApp extends StatelessWidget {
   const LegendCinemaApp({super.key});
@@ -10,15 +13,57 @@ class LegendCinemaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppConstant.appName,
-      theme: AppConstant.dark,
-      darkTheme: AppConstant.dark,
-      themeMode: ThemeMode.dark,
-      initialBinding: AppController(),
-      supportedLocales: AppConstant.localLanguages,
-      localizationsDelegates: AppConstant.delegate,
-      home: const SplashScreen(),
-    );
-  }
-}
+            debugShowCheckedModeBanner: false,
+            title: AppConstant.appName,
+            theme: darkTheme(const AppThemeOptions()),
+            darkTheme: AppConstant.dark,
+            themeMode: ThemeMode.dark,
+            initialBinding: AppController(),
+            supportedLocales: S.delegate.supportedLocales,
+            localizationsDelegates: AppConstant.delegate,
+            fallbackLocale: const Locale('km', 'KH'),
+            home: const SplashScreen(),
+          );
+        }
+      }
+
+// class LegendCinemaApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<Locale>(
+//       future: SharedPrefs.loadLocale(),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const CircularProgressIndicator();
+//         } else {
+//           if (snapshot.hasData) {
+//             Get.updateLocale(snapshot.data!); // Update locale immediately without restarting app
+//             return GetMaterialApp(
+//               debugShowCheckedModeBanner: false,
+//               title: AppConstant.appName,
+//               theme: AppConstant.dark,
+//               darkTheme: AppConstant.dark,
+//               themeMode: ThemeMode.dark,
+//               initialBinding: AppController(),
+//               supportedLocales: S.delegate.supportedLocales,
+//               localizationsDelegates: AppConstant.delegate,
+//               locale: snapshot.data,
+//               fallbackLocale: const Locale('km', 'KH'),
+//               home: SplashScreen(),
+//             );
+//           } else {
+//             // Handle error if unable to load locale
+//             return const Center(
+//               child: Text('Error loading locale'),
+//             );
+//           }
+//         }
+//       },
+//     );
+//   }
+// }
+
+
+
+
+
