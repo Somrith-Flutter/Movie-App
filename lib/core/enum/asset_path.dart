02e2878b906378e549ec4ba75753b24694ffd060
seyha <seyhaboy086@gmail.com>
2024-06-13 16:54:23 +0700

    imges homepage

---
+++ lib/core/enum/asset_path.dart
@@ -0,0 +1,24 @@
+class AssetPath {
+  static const String flagkhmer = 'assets/flages/flag_khmer.png';
+  static const String flagEngland = 'assets/flages/flag_england.png';
+  static const String flagChinese = 'assets/flages/flag_chinese.png';
+  static const String cinema1 = 'assets/images/cinema1.jpeg';
+  static const String cinema2 = 'assets/images/cinema2.jpeg';
+  static const String cinema3 = 'assets/images/cinema3.jpeg';
+  static const String cinema4 = 'assets/images/cinema4.jpeg';
+  static const String cinema5 = 'assets/images/cinema5.jpeg';
+  static const String cinema6 = 'assets/images/cinema6.jpeg';
+  static const String cinema7 = 'assets/images/cinema7.jpeg';
+  static const String cinema8 = 'assets/images/cinema8.jpeg';
+  static const String cinemaCard1 = 'assets/images/cinema_card.jpeg';
+  static const String fbhero = 'assets/images/f&b.jpeg';
+
+  static const String news1 = 'assets/images/news1.jpg';
+  static const String news2 = 'assets/images/news2.jpg';
+  static const String news3 = 'assets/images/news3.jpg';
+  static const String news4 = 'assets/images/news4.jpg';
+  static const String news5 = 'assets/images/news5.jpg';
+  static const String news6 = 'assets/images/news6.jpg';
+  static const String news7 = 'assets/images/news7.jpg';
+
+}
