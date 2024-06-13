09e3e7ffa61e3361e4e7249c99e5090c59662c07
ey_fat_pig <penheydeveloper@gmail.com>
2024-06-13 11:51:13 +0700

    cinema screen

--- lib/modules/landings/cinema/view/cinema_view.dart
+++ lib/modules/landings/cinema/view/cinema_view.dart
@@ -1,106 +1,149 @@
 import 'package:flutter/material.dart';
 import 'package:legend_cinema/config/themes/app_color.dart';
 import 'package:legend_cinema/constants/asset_path.dart';
-import 'package:legend_cinema/modules/landings/offers/view/offers_detail.dart';
 import 'package:legend_cinema/widgets/text_widget.dart';
-List<String> image = [
-  AssetPath.cinema2,
-  AssetPath.cinema6,
-  AssetPath.cinema4,
-  AssetPath.cinema5,
-  AssetPath.cinema7,
-];
 
-List<String> imageCard = [
-  AssetPath.cinemaCard1,
-  AssetPath.cinema6,
-  AssetPath.cinema7,
-];
-
-List<String> titles = [
-  'Special Offer 1',
-  'Special Offer 2',
-  'Special Offer 3',
-  'Special Offer 4',
-  'Special Offer 5',
-];
 class CinemaView extends StatelessWidget {
-  const CinemaView({super.key});
+  CinemaView({super.key});
+
+  final List<Map<String, String>> cinema = [
+    {
+      "image": AssetPath.cinema3, // Replace with your actual asset path
+      "title": "Legend Cinema 271 Mega Mall",
+      "subtitle": "3rd Floor, Chip Mong Mega Mall,",
+    },
+    {
+      "image": AssetPath.cinema2, // Replace with your actual asset path
+      "title": "Legend Cinema Sihanoukville",
+      "subtitle": "PGB-5-021, 4th Floor of prince",
+    },
+    {
+      "image": AssetPath.cinema1, // Replace with your actual asset path
+      "title": "Legend Eden Garden",
+      "subtitle": "City Center Boulevard, sangkat Sraschork, PhnomPenh City",
+    },
+    {
+      "image": AssetPath.cinema3, // Replace with your actual asset path
+      "title": "Movie 1",
+      "subtitle": "Subtitle 1",
+    },
+    {
+      "image": AssetPath.cinema2, // Replace with your actual asset path
+      "title": "Movie 2",
+      "subtitle": "Subtitle 2",
+    },
+    {
+      "image": AssetPath.cinema1, // Replace with your actual asset path
+      "title": "Movie 3",
+      "subtitle": "Subtitle 3",
+    },
+    {
+      "image": AssetPath.cinema3, // Replace with your actual asset path
+      "title": "Movie 1",
+      "subtitle": "Subtitle 1",
+    },
+    {
+      "image": AssetPath.cinema2, // Replace with your actual asset path
+      "title": "Movie 2",
+      "subtitle": "Subtitle 2",
+    },
+    {
+      "image": AssetPath.cinema1, // Replace with your actual asset path
+      "title": "Movie 3",
+      "subtitle": "Subtitle 3",
+    },
+    {
+      "image": AssetPath.cinema3, // Replace with your actual asset path
+      "title": "Movie 1",
+      "subtitle": "Subtitle 1",
+    },
+    {
+      "image": AssetPath.cinema2, // Replace with your actual asset path
+      "title": "Movie 2",
+      "subtitle": "Subtitle 2",
+    },
+    {
+      "image": AssetPath.cinema1, // Replace with your actual asset path
+      "title": "Movie 3",
+      "subtitle": "Subtitle 3",
+    },
+    // Add more items as needed
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         centerTitle: false,
-        title: const TextWidget('Cinema',size: 24,),
+        title: const TextWidget(
+          'Cinema',
+          size: 24,
+        ),
         flexibleSpace: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: AppColor.appbarColor,
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-            )
+            ),
           ),
         ),
       ),
-      body: Container(
-        color: Colors.black,
-        child: ListView(
+      body: Padding(
+        padding: const EdgeInsets.all(6),
+        child: ListView.builder(
           physics: const BouncingScrollPhysics(),
-          children: [
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: image.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailScreen(
-                          image: image[index],
-                          title: titles[index],
-                        ),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        border: Border.all(
-                          color: Colors.white70,
-                          width: 1,
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.asset(
-                              image[index],
-                              fit: BoxFit.fill,
-                              height: 200,
-                            ),
-                          ),
-                          ListTile(
-                            title: Text(
-                              titles[index],
-                              style: const TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+          itemCount: cinema.length,
+          itemBuilder: (context, index) {
+            final item = cinema[index];
+            return Container(
+              margin: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: Colors.white70,
+                  width: 1,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Image.asset(
+                      item["image"]!,
+                      fit: BoxFit.cover,
+                      height: 280, // Adjust height as per your requirement
                     ),
                   ),
-                );
-              },
-            ),
-          ],
+                  ListTile(
+                    title: TextWidget(
+                      item["title"]!,
+                      size: 20,
+                      bold: true,
+                    ),
+                    subtitle: Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                        ),
+                        const SizedBox(width: 4),
+                        Expanded(
+                          child: TextWidget(
+                            item["subtitle"]!,
+                            size: 16,
+                            bold: true,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
