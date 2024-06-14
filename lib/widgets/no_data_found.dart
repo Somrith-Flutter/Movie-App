de437ffafca708b45179fa6d6554ce3630847906
ey_fat_pig <penheydeveloper@gmail.com>
2024-06-14 09:21:57 +0700

    .

--- lib/widgets/no_data_found.dart
+++ lib/widgets/no_data_found.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:legend_cinema/constants/asset_path.dart';
+import 'package:legend_cinema/widgets/text_widget.dart';
 
 class NoDataFound extends StatelessWidget {
   const NoDataFound({super.key});
@@ -8,7 +9,14 @@ class NoDataFound extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(AssetPath.noData,fit: BoxFit.cover,)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(AssetPath.noData,fit: BoxFit.cover,),
+            const SizedBox(height: 5,),
+            TextWidget(" No Data Found",size: 20,color: Colors.grey[400],bold: true,)
+          ],
+        )
       ),
     );
   }
