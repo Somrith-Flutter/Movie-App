67246ee7d0d4bfe078bbf733ded098e3f6ab5a6a
lyhoung <pov.lyhoung@e-power.com.kh>
2024-06-15 14:53:33 +0700

    update

--- lib/core/bottom_navigation.dart
+++ lib/core/bottom_navigation.dart
@@ -7,7 +7,7 @@ import 'package:legend_cinema/modules/landings/offers/view/offers_page.dart';
 import 'package:legend_cinema/translation/generated/l10n.dart';
 
 class BottomNavigation extends StatefulWidget {
-  const BottomNavigation({Key? key}) : super(key: key);
+  const BottomNavigation({super.key});
 
   @override
   State<BottomNavigation> createState() => _BottomNavigationState();
@@ -49,14 +49,14 @@ class _BottomNavigationState extends State<BottomNavigation> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentPageIndex,
         onTap: _selectPage,
-        type: BottomNavigationBarType.fixed, // Set type to fixed to avoid shifting
+        type: BottomNavigationBarType.fixed, 
         backgroundColor: const Color.fromARGB(255, 26, 25, 25).withOpacity(0.9),
         items: [
-          _buildNavItem(Icons.home_outlined, S.of(context).home),
-          _buildNavItem(Icons.local_offer_outlined, S.of(context).offer),
-          _buildNavItem(Icons.location_pin, S.of(context).cinema),
-          _buildNavItem(Icons.food_bank_outlined, S.of(context).fb),
-          _buildNavItem(Icons.crop, S.of(context).more),
+          _buildNavItem(Icons.home_outlined, S.of(context).home, _currentPageIndex == 0),
+          _buildNavItem(Icons.local_offer_outlined, S.of(context).offer, _currentPageIndex == 1),
+          _buildNavItem(Icons.location_pin, S.of(context).cinema, _currentPageIndex == 2),
+          _buildNavItem(Icons.food_bank_outlined, S.of(context).fb, _currentPageIndex == 3),
+          _buildNavItem(Icons.crop, S.of(context).more, _currentPageIndex == 4),
         ],
       ),
     );
@@ -73,18 +73,19 @@ class _BottomNavigationState extends State<BottomNavigation> {
     });
   }
 
-  BottomNavigationBarItem _buildNavItem(IconData icon, String label) {
+  BottomNavigationBarItem _buildNavItem(IconData icon, String label, bool isSelected) {
     return BottomNavigationBarItem(
-      icon: Icon(icon),
-      label: label,
-      activeIcon: Container(
-        height: 20,
-        width: 35,
+      icon: Container(
+        height: 30,
+        width: 60,
         decoration: BoxDecoration(
-          color: Colors.red.withOpacity(0.5),
-          borderRadius: BorderRadius.all(Radius.circular(20))
+          color: isSelected ? Colors.red.shade900 : Colors.transparent,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          shape: BoxShape.rectangle,
         ),
-      )
+        child: Icon(icon, color: isSelected ? Colors.white : Colors.white.withOpacity(0.5)),
+      ),
+      label: label,
     );
   }
 }
