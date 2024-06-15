88657e7455abce1f7bd1d98b15f54d89d1910135
lyhoung <pov.lyhoung@e-power.com.kh>
2024-06-15 19:48:25 +0700

    -

--- lib/constants/api_path.dart
+++ lib/constants/api_path.dart
@@ -1,6 +1,6 @@
 class ApiPath{
   static const String newslist = 'api/new-activity/v1/list';
-  static const String locationlist = 'api/assets/location.json';
+  static const String locationlist = 'assets/location.json';
   static const String locationlistdata = 'api/f-n-b/v1/get-location-type';
   static const String aboutus = 'api/about-us/v1/u-get';
   static const String offers = 'api/offer/v1/offer-get';
