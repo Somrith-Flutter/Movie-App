dd5586c3defa98dc246c081e89229f3f21268429
ey_fat_pig <penheydeveloper@gmail.com>
2024-06-15 17:25:02 +0700

    cinema date show

--- lib/modules/landings/cinema/widgets/cinema_detail.dart
+++ lib/modules/landings/cinema/widgets/cinema_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:legend_cinema/config/themes/app_color.dart';
 import 'package:legend_cinema/widgets/back_widget.dart';
 import 'package:legend_cinema/widgets/text_widget.dart';
+import 'package:intl/intl.dart'; // Add this import for date formatting
 
 class CinemaDetail extends StatefulWidget {
   final String detailImage;
@@ -16,6 +17,7 @@ class CinemaDetail extends StatefulWidget {
 
 class _CinemaDetailState extends State<CinemaDetail> {
   bool isTextTapSelected = true;
+  String selectedDay = '';
 
   void _onTabTap(bool isTextSelected) {
     setState(() {
@@ -47,49 +49,108 @@ class _CinemaDetailState extends State<CinemaDetail> {
       body: Column(
         children: [
           Image.asset(widget.detailImage, height: 200, fit: BoxFit.cover),
-          Container(
-            width: double.infinity,
-            height: 50,
-            color: Colors.black,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          _buildTapSelected(),
+          Expanded(
+            child: isTextTapSelected
+                ? Column(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    _onTabTap(true);
-                  },
-                  child: TextWidget(
-                    "Now Showing",
-                    bold: true,
-                    color: isTextTapSelected ? Colors.white : Colors.grey,
-                  ),
+                Row(
+                  children: daysOfWeek.map((date) {
+                    return Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedDay = date;
+                          });
+                        },
+                        child: Container(
+                          height: 100,
+                          margin: const EdgeInsets.all(4),
+                          padding:
+                          const EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                            color: selectedDay == date
+                                ? Colors.blue
+                                : Colors.grey[800],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              date,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
                 ),
-                TextWidget("|", size: 30),
-                GestureDetector(
-                  onTap: () {
-                    _onTabTap(false);
-                  },
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: TextWidget(
-                    "Detail",
+                    selectedDay,
+                    size: 18,
                     bold: true,
-                    color: isTextTapSelected ? Colors.grey : Colors.white,
+                    color: Colors.blue,
                   ),
                 ),
               ],
+            )
+                : Center(child: TextWidget("Ah bek")),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTapSelected() {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      color: Colors.black,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: () {
+              _onTabTap(true);
+            },
+            child: TextWidget(
+              "Now Showing",
+              bold: true,
+              color: isTextTapSelected ? Colors.white : Colors.grey,
             ),
           ),
-          Expanded(
-            child: Container(
-              child: Column(
-                children: [
-                  isTextTapSelected?
-                      TextWidget("hahaha"):TextWidget("Ah bek")
-                ],
-              )
+          const TextWidget("|", size: 30),
+          GestureDetector(
+            onTap: () {
+              _onTabTap(false);
+            },
+            child: TextWidget(
+              "Detail",
+              bold: true,
+              color: isTextTapSelected ? Colors.grey : Colors.white,
             ),
           ),
         ],
       ),
     );
+  }
+
+  List<String> get daysOfWeek {
+    final now = DateTime.now();
+    final dateFormat = DateFormat('EEE, MMM d'); // Format: Day, Month Day
+    final days = List.generate(4, (i) => now.add(Duration(days: i)));
+
+    return days.map((date) {
+      if (date.isAtSameMomentAs(now)) {
+        return 'Today, ${DateFormat('MMM d').format(date)}'; // Format for Today
+      } else {
+        return dateFormat.format(date);
+      }
+    }).toList();
   }
 }
