a4ea204800d1b2dd993bd50996275b628a8fc8d4
lyhoung <pov.lyhoung@e-power.com.kh>
2024-06-15 22:26:18 +0700

    more

--- lib/modules/landings/more/view/more_page.dart
+++ lib/modules/landings/more/view/more_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:legend_cinema/config/routes/app_route.dart';
 import 'package:legend_cinema/config/themes/app_color.dart';
 import 'package:legend_cinema/constants/app_constant.dart';
+import 'package:legend_cinema/modules/auth/widget/register_form.dart';
 import 'package:legend_cinema/modules/landings/more/controller/more_controller.dart';
 import 'package:legend_cinema/modules/landings/more/repository/more_repository.dart';
 import 'package:legend_cinema/modules/landings/more/view/more_view.dart';
+import 'package:legend_cinema/shared/v_globle.dart';
 import 'package:legend_cinema/translation/generated/l10n.dart';
 import 'package:legend_cinema/widgets/text_widget.dart';
 
@@ -22,6 +25,28 @@ class MorePage extends StatelessWidget {
           centerTitle: false,
           title: TextWidget(S.of(context).account, size: 22, bold: true,),
           flexibleSpace: AppConstant.appbarTheme,
+          actions: [
+            if(accessToken.$.isNotEmpty)...[
+              GestureDetector(
+                onTap: () => AppRoute().push(context, const RegisterForm(numberPhone: '',)),
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  width: 85,
+                  height: 85,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    shape: BoxShape.circle,
+                    color: Colors.grey[200],
+                  ),
+                  child: const Icon(
+                    Icons.person,
+                    color: Colors.red,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ]
+          ],
         ),
         backgroundColor: AppColor.primaryColor,
         body: const MoreView(),
