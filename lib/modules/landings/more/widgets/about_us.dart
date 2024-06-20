6caca78130d264970f6a15a4ae571784f8145e8a
Leanghor <chhornleanghor0@gmail.com>
2024-06-20 09:20:44 +0700

    update

--- lib/modules/landings/more/widgets/about_us.dart
+++ lib/modules/landings/more/widgets/about_us.dart
@@ -9,6 +9,7 @@ import 'package:legend_cinema/translation/generated/l10n.dart';
 import 'package:legend_cinema/widgets/back_widget.dart';
 import 'package:legend_cinema/widgets/no_data_found.dart';
 import 'package:legend_cinema/widgets/text_widget.dart';
+
 class AboutUsWidget extends StatefulWidget {
   const AboutUsWidget({super.key});
 
@@ -17,7 +18,8 @@ class AboutUsWidget extends StatefulWidget {
 }
 
 class _AboutUsWidgetState extends State<AboutUsWidget> {
-  final MoreController controller = Get.put(MoreController(repository: MoreRepository()));
+  final MoreController controller =
+      Get.put(MoreController(repository: MoreRepository()));
 
   @override
   void initState() {
@@ -30,70 +32,90 @@ class _AboutUsWidgetState extends State<AboutUsWidget> {
     return Scaffold(
       appBar: AppBar(
         leading: const BackWidget(),
-        title: TextWidget(S.of(context).about_us, size: 22, bold: true,),
+        title: TextWidget(
+          S.of(context).about_us,
+          size: 22,
+          bold: true,
+        ),
         flexibleSpace: AppConstant.appbarTheme,
       ),
       body: Obx(() {
         if (controller.isLoading.value) {
-          return const Center(
-            child: CircularProgressIndicator()
-          );
+          return const Center(child: CircularProgressIndicator());
         } else if (controller.errorMessage.isNotEmpty) {
           return const NoDataFound();
         } else if (controller.aboutus.isEmpty) {
           return const Center(child: Text('No data available.'));
         } else {
           final about = controller.aboutus.first;
-          String cacheImage({String? img}){
-            if (AppConstant.baseIosIP == AppConstant.domainKey ){
+          String cacheImage({String? img}) {
+            if (AppConstant.baseIosIP == AppConstant.domainKey) {
               img = about.image;
             }
-            if (AppConstant.baseAndroidIP == AppConstant.domainKey){
+            if (AppConstant.baseAndroidIP == AppConstant.domainKey) {
               img = "${AppConstant.domainKey}/${about.image}";
-              debugPrint(img);
+              debugPrint("about us $img");
             }
             return img ?? '';
           }
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 220,
-                  width: double.maxFinite,
-                  child: CachedNetworkImage(
-                    imageUrl: cacheImage(),
-                    fit: BoxFit.cover,
-                    errorWidget: (context, url, error) => const Icon(Icons.error),
-                  ),
-                ),
-                const Gap(12),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: TextWidget(
-                    about.title ?? '',
-                    size: 24,
-                    bold: true,
-                  ),
-                ),
-                if (about.subtitle != null && about.subtitle!.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0,left: 12, top: 8),
-                    child: TextWidget(
-                      about.subtitle!,
-                      size: 18,
-                      color: Colors.grey,
+
+          return ListView.builder(
+            itemCount: controller.aboutus.length,
+            itemBuilder: (context, index) {
+              final us = controller.aboutus[index];
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(8.0),
+                          height: 175,
+                          width: 150,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(7),
+                            child: CachedNetworkImage(
+                              imageUrl: "${AppConstant.domainKey}/${us.image}",
+                              fit: BoxFit.fill,
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ),
+                        ),
+                        const Gap(12),
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextWidget(
+                                about.title ?? '',
+                                size: 22,
+                                bold: true,
+                              ),
+                              if (us.subtitle != null &&
+                                  us.subtitle!.isNotEmpty)
+                                TextWidget(
+                                  about.subtitle!,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                              TextWidget(
+                                '${us.description}',
+                                size: 16,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, top: 12),
-                  child: TextWidget(
-                    '    ${about.description}',
-                    size: 16,
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           );
         }
       }),
